18291b5aec1cd1a70beb11106fa2a2986d74323e
lpal88 <lpalpar893@g.educaand.es>
2023-04-30 13:32:29 +0200

    grid agenda

--- Agenda Visual/src/views/Create&modifyAgenda.vue
+++ Agenda Visual/src/views/Create&modifyAgenda.vue
@@ -1,8 +1,17 @@
 <template>
-    <div id="createAgenda__container">
-        <section class="background__profile"></section>
-        <section class="background__menu"></section>
-
+    <div class="background__profile"></div>
+    <button class="profile__button">
+        <span class="material-symbols-outlined">account_circle</span>
+    </button>
+    <div class="background__menu"></div>
+    <button class="menu__button">
+        <img class="menu__image" src="../assets/images/menu (1).svg" alt="botón menú principal">
+    </button>
+    <div id="agenda__container">
+        <h1 class="agenda__title">Crea una agenda</h1>
+        <section class="agenda__search"></section>
+        <section class="agenda__pictos"></section>
+        <section class="agenda__board"></section>
     </div>
 
 </template>
